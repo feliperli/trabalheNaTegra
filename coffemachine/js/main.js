2286a402e55c4ce75e85216c525f0b888125c87a
Felipe <feliperli@hotmail.com>
2016-11-04 10:31:51 -0200

    Added style prototype

---
+++ coffemachine/js/main.js
@@ -0,0 +1,51 @@
+function Numberone() {
+    document.getElementById("button1").innerHTML = "Hello World";
+}
+
+function Numbertwo() {
+    document.getElementById("button2").innerHTML = "Hello World";
+}
+
+function Numberthree() {
+    document.getElementById("button3").innerHTML = "Hello World";
+}
+
+function Numberfour() {
+    document.getElementById("button4").innerHTML = "Hello World";
+}
+
+function Numberfive() {
+    document.getElementById("button5").innerHTML = "Hello World";
+}
+
+function Numbersix() {
+    document.getElementById("button6").innerHTML = "Hello World";
+}
+
+function Numberseven() {
+    document.getElementById("button7").innerHTML = "Hello World";
+}
+
+function Numbereight() {
+    document.getElementById("button8").innerHTML = "Hello World";
+}
+
+function Numbernine() {
+    document.getElementById("button9").innerHTML = "Hello World";
+}
+
+function Numberzero() {
+    document.getElementById("button0").innerHTML = "Hello World";
+}
+
+function Enter() {
+    document.getElementById("buttonEnter").innerHTML = "Hello World";
+}
+
+function Cancel() {
+    document.getElementById("buttonCancel").innerHTML = "Hello World";
+}
+
+function Clear() {
+    document.getElementById("buttonClear").innerHTML = "Hello World";
+}
